60565b83df68f87a8fe381b57d5555e709c290c6
Robert McCrary <rmccrar6@students.kennesaw.edu>
2024-08-21 16:18:18 -0400

    roman to integer

---
+++ RomanToInteger/Solution.java
@@ -0,0 +1,39 @@
+package RomanToInteger;
+
+import java.util.HashMap;
+import java.util.*;
+
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int total = 0;
+        int length = s.length();
+
+        for (int i = 0; i < length; i++) {
+            int currentValue = map.get(s.charAt(i));
+
+            int nextValue = 0;
+
+            if (i + 1 < length) {
+                nextValue = map.get(s.charAt(i + 1));
+            }
+
+            if (currentValue < nextValue) {
+                total -= currentValue;
+            } else {
+                total += currentValue;
+            }
+        }
+
+        return total;
+    }
+}
